6d451f92de650ac2935c80fb109d306ed1694764
zhaozhuang <zzzhaozzzz@163.com>
2021-04-28 14:04:15 +0800

    fix:test

--- test/helpers/dispatch.ts
+++ test/helpers/dispatch.ts
@@ -1,4 +1,5 @@
-import { AnyAction, DispatchFun } from "../../src/types/interface";
+import { DispatchFun } from "../../src/types/dispatch";
+import { AnyAction } from "../../src/types/interface";
 
 export let dispatch: DispatchFun<any> = function (
     action: AnyAction,
